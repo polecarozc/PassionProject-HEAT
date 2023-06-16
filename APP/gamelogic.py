17b3602955d2cb34be47512b19d23c9a4e0bc57a
polecarozc <zcpolecaro@gmail.com>
2023-06-16 12:57:30 -0400

    working on perfect basic strategy with hard totals, adding logic to add usable splits

--- APP/gamelogic.py
+++ APP/gamelogic.py
@@ -1,11 +1,10 @@
 from players import player
 from decks import deck
-from perfect_basic_strategy import pbs
+import perfect_basic_strategy as pbs
 class game_logic:
     def __init__(self,bankroll, shoe_size, minbet, maxbet, shuffler):
         self.decks = deck()
         self.decks.create_deck()
-        self.pbs = pbs()
         self.player = player(False, self.decks)
         self.dealer = player(True, self.decks)
         self.bankroll = bankroll
@@ -14,19 +13,50 @@ class game_logic:
         self.maxbet = maxbet
         self.shuffler = shuffler
         self.bet = 0
+        self.playable_hands = []
+        
+    def total_split_score(self, hands):
+        total = 0
+        for list in self.playable_hands[hands]:
+            for card in list:
+                total += card.total()
+        return total
 
     def check_split(self):
         if self.player.cards[0].amount == self.player.cards[1].amount:
             self.qualify_split()
         else:
-            print('cards are not the same, player cards are {0} & {1}'.format(self.player.cards[0].amount,self.player.cards[1].amount))
-        
+            print('cards are not the same, player cards are {0} & {1}'.format(self.player.cards[0].value,self.player.cards[1].value))
+
     def qualify_split(self):
-        if pbs.perfect_split(self.player.cards[0].amount, self.dealer.cards[0].amount)==1:
-            print('--------CARDS WILL BE SPLIT, players cards are {} & {}, dealers card is {}'.format(self.player.cards[0].amount,self.player.cards[1].amount, self.dealer.cards[0].amount))
+        if pbs.perfect_split(self.player.cards[0].amount, self.dealer.cards[0].amount) == 1:
+            print('--------CARDS WILL BE SPLIT, players cards are {} & {}, dealers card is {}'.format(self.player.cards[0].value,self.player.cards[1].value, self.dealer.cards[0].value))
+            self.playable_hands.append(self.player.cards[0])
+            self.playable_hands.append(self.player.cards[1])
+
+            print('----TEST, score for first split hand is {}'.format(self.playable_hands[0].total()))
+
+
+            for i in range(2):
+                total = 0
+                for list in self.playable_hands[i]:
+                    for card in list:
+                        total += card.total()
+
+                while pbs.hard_total(total) !=1:
+                    self.player.cards.clear()
+                    self.player.hit()
+                    self.playable_hands.append(self.player.cards[i])
+
+
+
+
+
+            print("players new hands are {} & {}".format(self.playable_hands[0].value, self.playable_hands[1].value))
+
         else:
-            print('--------cards will not be split, players cards are {} & {}, dealers card is {}'.format(self.player.cards[0].amount,self.player.cards[1].amount, self.dealer.cards[0].amount))
-        
+            print('--------cards will not be split, players cards are {} & {}, dealers card is {}'.format(self.player.cards[0].value,self.player.cards[1].value, self.dealer.cards[0].value))
+
 
 
     def check_ace(self):
--- APP/perfect_basic_strategy.py
+++ APP/perfect_basic_strategy.py
@@ -1,19 +1,17 @@
 
-class pbs():
-    """
-    there is probably a much better and nicer looking way to do this
-    """
-    def perfect_split(player_card, dealer_card):
-        if player_card in [1, 8]:
-            return 1
-        elif player_card == 9 and dealer_card in [2,3,4,5,6,8,9]:
-            return 1
-        elif player_card == 7 and dealer_card in [2,3,4,5,6,7]:
-            return 1
-        elif player_card == 6 and dealer_card in [3,4,5,6]:
-            return 1
-        elif player_card in [2,3] and dealer_card in [4,5,6,7]:
-            return 1
-        else:
-            return 2
-        
+def perfect_split(player_card, dealer_card):
+    if player_card in [1, 8]:
+        return 1
+    elif player_card == 9 and dealer_card in [2,3,4,5,6,8,9]:
+        return 1
+    elif player_card == 7 and dealer_card in [2,3,4,5,6,7]:
+        return 1
+    elif player_card == 6 and dealer_card in [3,4,5,6]:
+        return 1
+    elif player_card in [2,3] and dealer_card in [4,5,6,7]:
+        return 1
+    else:
+        return 2
+
+def hard_total(player_total):
+    pass
